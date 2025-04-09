1ed1d418124b84ae868a931496d1821c8322cb10
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-04-09 11:52:31 +0200

    Correction Object

--- Langage/ex-jeu-objet.js
+++ Langage/ex-jeu-objet.js
@@ -3,57 +3,69 @@
 // les 4 fonctions ci-dessous :
 // Il faudra appeler la fonction getRandomInt :
 // Random.getRandomInt(0, 100)
-function getRandom() {
-  return Math.random();
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
-function getRandomIntInclusive(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-}
+const Random = {
+  getRandom() {
+    return Math.random();
+  },
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  },
+  getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  },
+};
 
 const readline = require('node:readline');
 
-
-function jouer() {
-  if (essais.length) {
-    console.log('Vous avez déjà essayé : ' + essais.join(', '));
+class Jeu {
+  constructor(options = {}) {
+    // Nullish coalescing operator
+    const min = options.min ?? 0;
+    const max = options.max ?? 100;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.entierAlea = Random.getRandomInt(min, max);
+    this.essais = [];
   }
-  rl.question('Saisissez un nombre entre 0 et 100 : ', (saisie) => {
-    console.log('Vous avez saisi :', saisie);
-
-    const entierSaisi = Number.parseInt(saisie, 10);
-
-    if (Number.isNaN(entierSaisi)) {
-      console.log("Ce n'est pas un nombre !");
-      return jouer();
+  jouer() {
+    if (this.essais.length) {
+      console.log('Vous avez déjà essayé : ' + this.essais.join(', '));
     }
+    this.rl.question('Saisissez un nombre entre 0 et 100 : ', (saisie) => {
+      console.log('Vous avez saisi :', saisie);
 
-    essais.push(entierSaisi);
+      const entierSaisi = Number.parseInt(saisie, 10);
 
-    if (entierSaisi < entierAlea) {
-      console.log('Trop petit !');
-      jouer();
-    } else if (entierSaisi > entierAlea) {
-      console.log('Trop grand !');
-      jouer();
-    } else {
-      console.log('Bravo ! Vous avez trouvé le nombre !');
-      rl.close();
-    }
-  });
+      if (Number.isNaN(entierSaisi)) {
+        console.log("Ce n'est pas un nombre !");
+        return this.jouer();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit !');
+        this.jouer();
+      } else if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand !');
+        this.jouer();
+      } else {
+        console.log('Bravo ! Vous avez trouvé le nombre !');
+        this.rl.close();
+      }
+    });
+  }
 }
+
 
 // Exercice 2
 // Créer une classe Jeu
@@ -65,13 +77,9 @@ function jouer() {
 // jeu.jouer();
 // A ce stade le constructeur ne prend pas de paramètres
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-const entierAlea = getRandomInt(0, 100);
-const essais = [];
-jouer();
+
+const jeu = new Jeu();
+jeu.jouer();
 
 // Exercice 3
 // Modifier le constructeur de Jeu pour qu'il prenne
