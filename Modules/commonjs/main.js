d56413ba59ef7bbe583bd7b42749f317bd6d2855
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-04-09 12:29:36 +0200

    CommonJS

---
+++ Modules/commonjs/main.js
@@ -0,0 +1,7 @@
+const hello = require('./hello.js');
+const MyMath = require('./my-math.js');
+
+console.log(hello('World'));
+console.log(MyMath.sum(1, 2));
+console.log(MyMath.multiply(2, 3));
+
---
+++ Modules/commonjs/my-math.js
@@ -0,0 +1,8 @@
+exports.sum = function sum(a, b) {
+  return a + b;
+}
+
+exports.multiply = function multiply(a, b) {
+  return a * b;
+}
+
