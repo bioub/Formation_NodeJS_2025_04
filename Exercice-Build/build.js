cab878187a66a726c6d06a30bf09eef074526f2f
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-04-10 13:27:09 +0200

    Terser

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -1,8 +1,6 @@
-const fs = require('fs-extra');
 const path = require('path');
-const del = require('del');
 const md5 = require('md5');
-const UglifyJS = require('uglify-es');
+const { minify } = require('terser');
 
 const distPath = path.resolve(__dirname, 'dist');
 const srcPath = path.resolve(__dirname, 'src');
